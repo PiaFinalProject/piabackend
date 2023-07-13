c5e0f67b737b5727af7f83fdc0ade11dbb7a14e8
Çağrı KART <ckarte43@gmail.com>
2023-07-13 15:34:52 +0300

    pia
    pia

---
+++ src/main/java/com/teknokafalar/piabackend/PiabackendApplication.java
@@ -0,0 +1,13 @@
+package com.teknokafalar.piabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiabackendApplication.class, args);
+    }
+
+}
